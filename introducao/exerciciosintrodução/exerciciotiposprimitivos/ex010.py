6cf8fa5c5b72e8974bf7ad09df0bdc2cde0c0c0b
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-05-10 09:23:04 -0300

    feat: Reorganizando ex 010

--- introducao/exerciciosintrodução/exerciciotiposprimitivos/ex010.py
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex010.py
@@ -1,4 +1,9 @@
-tamanho = float(input('Digite o tamanho'))
+tamanho = float(input('Uma distância em metros: '))
 conversorCentimetro = tamanho * 100
 conversorMilimetro = tamanho * 1000
-print('A conversão de {} metros em centimetros é igual a: {} centimetros\n em milimetros é igual a: {} milimetros'.format(tamanho,conversorCentimetro,conversorMilimetro))
+conversorKM = tamanho / 1000
+conversorHectometros = tamanho / 100
+conversorDec = tamanho / 10
+conversorDecimetros = tamanho * 10
+print('A medida de {}m corresponde a\n{}cm\n{}mm\n{}KM\n{}HM'.format(tamanho,conversorCentimetro,conversorMilimetro,conversorKM,conversorHectometros))
+print('{}DAM\n{}dm'.format(conversorDec,conversorDecimetros))

4ef2b8f1e33423ad55d4c77e54345c8a04ab9701
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-05-10 10:16:19 -0300

    feat: Reorganizando ex 012

--- introducao/exerciciosintrodução/exerciciotiposprimitivos/ex012.py
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex012.py
@@ -1,4 +1,5 @@
-saldo = float(input('Qual o seu saldo: '))
-dolarpreco = 4.92
-conversor = saldo/dolarpreco
-print('Você tem {} reais, você poderá comprar {:.2f} dolares'.format(saldo, conversor))
+saldo = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolarpreco = saldo/4.92
+europreco = saldo/5.80
+print('Com {:.2f}, você pode comprar US${:.2f}'.format(saldo,dolarpreco))
+print('Com {:.2f} você pode comprar EU€{:.2f}'.format(saldo,europreco))

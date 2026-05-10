1b0fddb84db704a3dc9b007f9874573af141d208
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-05-10 10:34:35 -0300

    feat: Reorganizando ex 014

--- introducao/exerciciosintrodução/exerciciotiposprimitivos/ex014.py
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex014.py
@@ -1,4 +1,4 @@
-preco_produto = float(input('Digite o preço do produto: '))
+preco_produto = float(input('Qual é o preço do produto? R$'))
 desconto =float(input('Dono da loja digite o desconto do produto: '))
 aplicando_desconto = preco_produto - (preco_produto * desconto)
-print('O produto do valor de {} reais será aplicado um desconto de {} %, e será igual a {}'.format(preco_produto, desconto, aplicando_desconto))
+print('O produto que custava {:.2f}, na promoção com desconto de {:.2f}% vai custar R${:.2f}'.format(preco_produto, desconto, aplicando_desconto))

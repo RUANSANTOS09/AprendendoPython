9401aeb6c997f845808378e4d7a0a1a69c56cbd9
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-05-10 10:22:21 -0300

    feat: Reorganizando ex 013

--- introducao/exerciciosintrodução/exerciciotiposprimitivos/ex013.py
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex013.py
@@ -2,4 +2,5 @@ largura = float(input('Qual a largura da parede? '))
 altura = float(input('Qual a altura da parede? '))
 area = largura*altura
 tinta_necessaria = area/2
-print('A area é igual a {} metros quadrados e a quantidade de tinta necessaria sera de {:.2f} litros'.format(area, tinta_necessaria))
+print('Sua parede tem a dimensão de {} x {} e sua área é de {:.2f}m\u00B2.'.format(largura,altura,area))
+print('Para pintar essa parede, você precisará de {:.2f}L de tinta.'.format(tinta_necessaria))

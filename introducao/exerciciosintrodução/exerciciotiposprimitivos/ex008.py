8267e73dc36b0abde37fc5e2e9dee5655dda5539
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-05-10 08:23:44 -0300

    feat: Reorganizando ex 008

--- introducao/exerciciosintrodução/exerciciotiposprimitivos/ex008.py
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex008.py
@@ -2,4 +2,4 @@ num = int(input('Digite um numero: '))
 dobr = num * 2
 trip = num * 3
 raiz = num ** (1/2)
-print('O dobro de {} é igual a: {}\n O triplo é igual a: {}\n A raiz quadrada é: {}'.format(num, dobr, trip, raiz))
+print('O dobro de {} é igual a: {}\n O triplo de {} é igual a: {}\n A raiz quadrada de {} é: {:.2f}'.format(num, dobr, num, trip, num, raiz))

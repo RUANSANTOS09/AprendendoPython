e957ae209ff13fe10f12c91b03268c05092082a4
Ruan Santos Sacramento <ruansantossacramento32@gmail.com>
2026-05-10 09:52:46 -0300

    feat: Reorganizando ex 011

--- introducao/exerciciosintrodução/exerciciotiposprimitivos/ex011.py
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex011.py
@@ -1,2 +1,13 @@
-num = int(input('Digite um numero: '))
-print('A tabuada de {} é igual a {},{},{},{},{},{},{},{},{},{}'.format(num, num*1,num*2,num*3,num*4,num*5,num*6,num*7,num*8,num*9,num*10))
+num = int(input('Digite um número para ver sua tabuada: '))
+n1 = num * 1
+n2 = num * 2
+n3 = num * 3
+n4 = num * 4
+n5 = num * 5
+n6 = num * 6
+n7 = num * 7
+n8 = num * 8
+n9 = num * 9
+n10 = num * 10
+print('{:_<12}\n{:2} x 1 = {}\n{:2} x 2 = {}\n{:2} x 3 = {}\n{:2} x 4 = {}\n{:2} x 5 = {}'.format("",n1,n1,num,n2,num,n3,num,n4,num,n5))
+print('{:2} x 6 = {}\n{:2} x 7 = {}\n{:2} x 8 = {}\n{:2} x 9 = {}\n{:2} x 10 = {}\n{:_<12}'.format(num,n6,num,n7,num,n8,num,n9,num,n10,""))

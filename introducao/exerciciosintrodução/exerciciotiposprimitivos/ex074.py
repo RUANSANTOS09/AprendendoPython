c93d48fa84328a1978e0920f45e210146441acf1
ruans <ruansantossacramento32@gmail.com>
2026-05-14 18:36:08 -0300

    feat: novo exercicio na pasta exercicotipoprimitivos

---
+++ introducao/exerciciosintrodução/exerciciotiposprimitivos/ex074.py
@@ -0,0 +1,8 @@
+nome = 'Ruan'
+idade = 18
+altura = 1.70
+estudante = True
+print(type(nome), nome)
+print(type(idade), idade)
+print(type(altura), f'{altura:.2f}')
+print(type(estudante), estudante)

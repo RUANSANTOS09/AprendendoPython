053c8914924ff4ee22c9e9693ee651b146b292be
ruans <ruansantossacramento32@gmail.com>
2026-05-16 09:55:15 -0300

    feat: nova pasta modulos com novas aulas

---
+++ introducao/modulos/aula010.py
@@ -0,0 +1,4 @@
+from math import sqrt,ceil,floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz quadrada de {num} é {floor(raiz)}') #Com o metodo ceil, ele arredonda pra cima ja com floor ele arredonda pra baixo

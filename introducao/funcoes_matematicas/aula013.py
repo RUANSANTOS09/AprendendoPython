3bb9d862bc63f6c712e684dd1be1d23f8a5efc2b
ruans <ruansantossacramento32@gmail.com>
2026-05-25 12:59:23 -0300

    feat: novas pasta funcoes_matematicas com novas aulas

---
+++ introducao/funcoes_matematicas/aula013.py
@@ -0,0 +1,21 @@
+# Funções builtin
+import math
+valores = [2,3,4,5,-2,-6,1,-4,-4,3,-3]
+print(max(valores))
+print(min(valores))
+a = - 5
+b = 4
+print(abs(a))
+print(pow(a,b))
+c = 2.78900
+print(round(c,3))
+
+x = 8
+y = 100
+# raiz_quadrada = math.sqrt(x)
+# print(math.ceil(raiz_quadrada))
+# logaritmo = math.log10(y)
+# print(logaritmo)
+print(math.pi)
+print(math.factorial(x))
+print(x / math.inf)

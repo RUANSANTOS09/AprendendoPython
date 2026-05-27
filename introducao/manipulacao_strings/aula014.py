f28cea0fb29a6cdba1b1f23b53f02c61bdc008a2
ruans <ruansantossacramento32@gmail.com>
2026-05-26 22:49:52 -0300

    feat: novas pasta manipulação_de_strings com novas aulas

---
+++ introducao/manipulacao_strings/aula014.py
@@ -0,0 +1,40 @@
+# # # # frase = 'Vamos aprender Python hoje'
+# # # # # palavras = frase.split()
+# # # # # print(palavras)
+# # # # # for letra in frase:
+# # # # #     print(letra)
+# # # # print(frase[0:5])
+# # # # email = input('Digite seu email: ')
+# # # # arroba = email.find('@')
+# # # # print(arroba)
+# # # # usuario = email[0:arroba]
+# # # # print(f'Nome do usuário: {usuario}')
+# # # # dominio = email[arroba+1:]
+# # # # print(f'Domínio: {dominio}')
+# # # produtos = 'Carbonato de sódio e óxido de zinco'
+# # # print('sódio' in produtos)
+# # # print('magnésio' not in produtos)
+# # # item = 'hipoclorito'
+# # # pos = item.find('clor')
+# # # print(pos)
+# # # pos = item.find('flu')
+# # # print(pos)
+# # # objeto_celeste = 'Galáxia esPiral M31'
+# # # print(objeto_celeste.upper())
+# # # print(objeto_celeste.lower())
+# # # print(objeto_celeste.capitalize())
+# # # print(objeto_celeste.title())
+# # suplemento = 'Cloreto de Magnésio'
+# # new_suplemento = suplemento.replace('Magnésio', 'Zinco')
+# # print(suplemento)
+# # print(new_suplemento)
+# frase = '          Ômega três é bom para a saúde!    '
+# print(frase)
+# print(frase.lstrip())
+# print(frase.lstrip())
+# print(frase.strip())
+fruta = 'Abacate'
+print(fruta)
+print(fruta.rjust(20))
+print(fruta.center(20))
+print(fruta.ljust(20))

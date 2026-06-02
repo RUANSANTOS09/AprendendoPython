d6670379d05e69ddc77affb47533aad94519be6e
ruans <ruansantossacramento32@gmail.com>
2026-06-02 15:33:00 -0300

    feat: novos aulas na pasta funções

---
+++ introducao/funcoes/aula017.py
@@ -0,0 +1,45 @@
+# Funções
+# Modularizção, Reúso de código, Legibilidade
+def mensagem():
+   print('Hello World')
+   print('Engenheiro de Dados')
+
+mensagem()
+
+# FUnção com argumentos
+def soma(num1,num2):
+  print(num1 + num2)
+
+
+soma(4,4)
+
+def mult (x,y):
+  return x * y
+
+a = 5
+b = 7
+c = mult(a,b)
+print(f'O produto de {a} e {b} é {c}')
+
+def div(k,j):
+    if j != 0:
+        return k / j
+    else:
+      return 'Impossivél dividir um número por zero!'
+if __name__ == '__main__':
+    a = int(input('Digite um número: '))
+    b = int(input('Digite outro número: '))
+    r = div(a,b)
+    print(f'{a} dividido por {b} é igual a {r}')
+
+def quadrado(val):
+    quadrados = []
+    for x in val:
+        quadrados.append(x ** 2)
+    return quadrados
+
+if __name__ == '__main__':
+    valores = [2,5,7,9,12]
+    resulados = quadrado(valores)
+    for x in resulados:
+        print(x)

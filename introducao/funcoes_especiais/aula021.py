0d195cb1ca354bd18a48c79719e4f6cb15b2d66f
ruans <ruansantossacramento32@gmail.com>
2026-06-15 10:15:10 -0300

    feat: novos conceitos aprendidos

--- introducao/funcoes_especiais/aula021.py
+++ introducao/funcoes_especiais/aula021.py
@@ -25,6 +25,41 @@ print(f_c(90))
 # Sintaxe
 # map(função, iterável)
 
-num = [1,2,3,4,5,6,7,8]
-dobro = list(map(lambda x : x*2, num)
+# num = [1,2,3,4,5,6,7,8]
+# # dobro = list(map(lambda x : x*2, num))
+# # print(dobro)
 
+# palavras = ['Python', 'é', 'uma','linguagem', 'de', 'programação']
+# maiusculas = list(map(str.upper, palavras))
+# print(maiusculas)
+
+# Função filter()
+# Sintaxe
+# filter(função, sequência)
+
+# def numeros_pares(n):
+#     return n % 2 == 0
+#
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# num_par = list(filter(numeros_pares, numeros))
+# print(num_par)
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# num_impar = list(filter(lambda x: x % 2  != 0, numeros))
+# print(num_impar)
+
+
+# Função reduce()
+# Sintaxe
+# reduce(função, sequencia, valor_inicial)
+from functools import reduce
+# def mult(x,y):
+#     return x*y
+# numeros = [1,2,3,4,8,6]
+# total = reduce(mult, numeros)
+# print(total)
+
+# Soma cumulativa dos quadrados de valores, usando expressaão lamda
+numeros = [1,2,3,4]
+# ((1² + 2²)² + 3²)² + 4²
+total = reduce(lambda x,y : x **2 + y**2, numeros)
+print(total) 

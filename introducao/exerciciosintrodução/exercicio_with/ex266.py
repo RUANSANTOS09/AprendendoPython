881d64b5c1bb25c37a81741830f67bd549eeb5dd
ruans <ruansantossacramento32@gmail.com>
2026-06-22 23:12:53 -0300

    feat: novos arquivos de leitura

--- introducao/exerciciosintrodução/exercicio_with/ex266.py
+++ introducao/exerciciosintrodução/exercicio_with/ex266.py
@@ -2,4 +2,4 @@ r = input('Digite um termo: ')
 with open('product.txt', 'r', encoding='utf-8') as file:
     for index, value in enumerate(file,start=1):
         if r in value:
-         print(f'Encontrado na posição {index}: {value}')
+         print(f'Termo encontrado na posição {index}: {value}')
